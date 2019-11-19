dbdb9079e6245b1fdc78fc81bd627a19d3df22eb
mengdezhi <83398609@qq.com>
2019-11-19 19:04:09 +0800

    添加自动刷新页面脚本

---
+++ shell/refresh.php
@@ -0,0 +1,5 @@
+<?php
+    include dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib.php';
+    Core_View::instance()->render();
+?>
+<html><head><meta http-equiv="refresh" content="3"></head><body></body></html>
